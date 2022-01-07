63d818f710c707679d8faad4d2dce67d930d3b54
Presley Duggan <57386908+presleyduggan@users.noreply.github.com>
2022-01-07 09:03:25 -0800

    added start and end
    thanks poles

--- src/pages/Fame.jsx
+++ src/pages/Fame.jsx
@@ -1,47 +1,44 @@
-import React, {useEffect} from 'react';
-import Stonkbar from '../components/Stonkbar';
-import Josh2021 from '../images/fame/josh.JPG';
-import "../styles/Fame.css"
-
-
-
+import React, { useEffect } from "react";
+import Stonkbar from "../components/Stonkbar";
+import Josh2021 from "../images/fame/josh.JPG";
+import "../styles/Fame.css";
 
 function Stonks() {
+  useEffect(() => {
+    document.title = "Stonks Hall of Fame";
+  }, []);
 
-    useEffect(() => {
-        document.title = "Stonks Hall of Fame"
-     }, []);
+  return (
+    <div>
+      <Stonkbar></Stonkbar>
+      <br />
+      <br />
+      <br />
+      <h1>Stonks Hall of Fame</h1>
+      <br></br>
+      <br></br>
+      <br></br>
 
-
-    return ( 
-        <div>
-            <Stonkbar></Stonkbar>
-            <br/>
-            <br/>
-            <br/>
-            <h1>Stonks Hall of Fame</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-
-            <h1>2021</h1>
-            <h3>Joshua Moore</h3>
-            <h3>$F</h3>
-            <h3 style={{color: "green"}}>130.78%</h3>
-            <img src={Josh2021} alt="Josh 2021" height={400} width={500}/>
-            <br></br>
-            <br></br>
-            <p>“As betting at the race ring adds neither strength nor speed to the horse, 
-                so the exchange of shares in the stock market adds no capital to business, 
-                no increase in the production and no purchasing power to the market.” -Henry Ford, 1926</p>
-            <br></br>
-            <br></br>
-            <br></br>
-
-
-
-        </div>
-     );
+      <h1>2021</h1>
+      <h3>Joshua Moore</h3>
+      <h3>$F</h3>
+      <h3>Start Price: $9.00</h3>
+      <h3>End Price: $20.77</h3>
+      <h3 style={{ color: "green" }}>130.78%</h3>
+      <img src={Josh2021} alt="Josh 2021" height={400} width={500} />
+      <br></br>
+      <br></br>
+      <p>
+        “As betting at the race ring adds neither strength nor speed to the
+        horse, so the exchange of shares in the stock market adds no capital to
+        business, no increase in the production and no purchasing power to the
+        market.” -Henry Ford, 1926
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
 }
 
 export default Stonks;
--- src/pages/Shame.jsx
+++ src/pages/Shame.jsx
@@ -1,44 +1,43 @@
-import React, {useEffect} from 'react';
-import Stonkbar from '../components/Stonkbar';
-import "../styles/Shame.css"
-import Rex2021 from '../images/shame/rex3.jpg';
-
-
+import React, { useEffect } from "react";
+import Stonkbar from "../components/Stonkbar";
+import "../styles/Shame.css";
+import Rex2021 from "../images/shame/rex3.jpg";
 
 function Stonks() {
+  useEffect(() => {
+    document.title = "Stonks Hall of Shame";
+  }, []);
 
-    useEffect(() => {
-        document.title = "Stonks Hall of Shame"
-     }, []);
+  return (
+    <div>
+      <Stonkbar></Stonkbar>
+      <br />
+      <br />
+      <br />
+      <h1>Stonks Hall of Shame</h1>
+      <br></br>
+      <br></br>
+      <br></br>
 
-
-    return ( 
-        <div>
-            <Stonkbar></Stonkbar>
-            <br/>
-            <br/>
-            <br/>
-            <h1>Stonks Hall of Shame</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-
-            <h1>2021</h1>
-            <h3>Rex W.</h3>
-            <h3>$AJAX/$CZOO</h3>
-            <h3 style={{color: "red"}}>-47.57%</h3>
-            <p>$SPY was up <span style={{color: "green"}}>&nbsp;28.79%&nbsp;</span> that year</p>
-            <img src={Rex2021} alt="Rex 2021" height={350} width={350}/>
-            <br></br>
-            <br></br>
-            <p>“Wait, I thought you said stonks only go up”</p>
-            <br></br>
-            <br></br>
-            <br></br>
-
-
-        </div>
-     );
+      <h1>2021</h1>
+      <h3>Rex W.</h3>
+      <h3>$AJAX/$CZOO</h3>
+      <h3>Start Price: $11.52</h3>
+      <h3>End Price: $6.04</h3>
+      <h3 style={{ color: "red" }}>-47.57%</h3>
+      <p>
+        $SPY was up <span style={{ color: "green" }}>&nbsp;28.79%&nbsp;</span>{" "}
+        that year
+      </p>
+      <img src={Rex2021} alt="Rex 2021" height={350} width={350} />
+      <br></br>
+      <br></br>
+      <p>“Wait, I thought you said stonks only go up”</p>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
 }
 
 export default Stonks;
