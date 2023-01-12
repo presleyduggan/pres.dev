8bddf35ec70b96a7b46ed7e95ebb8471cdbae955
Presley Duggan <57386908+presleyduggan@users.noreply.github.com>
2023-01-12 08:39:15 -0800

    hall of fame/shame 2022

--- src/pages/Fame.jsx
+++ src/pages/Fame.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Stonkbar from "../components/Stonkbar";
 import Josh2021 from "../images/fame/josh.JPG";
+import Sean2022 from "../images/fame/sean.jpg";
 
 function Stonks() {
   useEffect(() => {
@@ -41,6 +42,34 @@ function Stonks() {
             horse, so the exchange of shares in the stock market adds no capital
             to business, no increase in the production and no purchasing power
             to the market.” -Henry Ford, 1926
+          </p>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div>
+        <h1 className="text-4xl font-semibold">2022</h1>
+        <div className="text-xl font-semibold">
+          <h3>Sean Flignor</h3>
+          <h3>$IBM</h3>
+          <h3>Start Price: $133.66</h3>
+          <h3>End Price: $140.89</h3>
+        </div>
+        <h3 className="text-green-500">5.41%</h3>
+        <div>
+          <img
+            className="flex content-center mx-auto"
+            src={Sean2022}
+            alt="Sean2022"
+            height={400}
+            width={500}
+          />
+          <br></br>
+          <br></br>
+          <p className="text-black text-lg">
+            “Water Covers Two-Thirds of the Earth… I cover the rest” -Deion
+            Sanders
           </p>
         </div>
       </div>
--- src/pages/Shame.jsx
+++ src/pages/Shame.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Stonkbar from "../components/Stonkbar";
 import "../styles/Shame.css";
 import Rex2021 from "../images/shame/rex3.jpg";
+import Poles2022 from "../images/shame/poles.jpeg";
 
 function Stonks() {
   useEffect(() => {
@@ -41,6 +42,32 @@ function Stonks() {
       <br></br>
       <br></br>
       <p>“Wait, I thought you said stonks only go up”</p>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <h1 className="text-4xl font-semibold">2022</h1>
+      <div className="text-xl font-semibold">
+        <h3>Alex P</h3>
+        <h3>$ROKU</h3>
+        <h3>Start Price: $228.2</h3>
+        <h3>End Price: $40.70</h3>
+      </div>
+      <h3 className="text-red-500">-82.16%</h3>
+      <p>
+        $SPY was down <span className="text-red-500">&nbsp;-19.48%&nbsp;</span>{" "}
+        that year
+      </p>
+      <img
+        src={Poles2022}
+        className="flex content-center mx-auto"
+        alt="Poles 2022"
+        height={350}
+        width={350}
+      />
+      <br></br>
+      <br></br>
+      <p>“I don’t even like dumbass Roku”</p>
       <br></br>
       <br></br>
       <br></br>
