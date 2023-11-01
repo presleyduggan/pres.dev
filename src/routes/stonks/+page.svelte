1e22c2c61613dc133be4671ddf48a000b0b1defd
presley <57386908+presleyduggan@users.noreply.github.com>
2023-11-01 00:56:15 -0600

    stonks table is working

--- src/routes/stonks/+page.svelte
+++ src/routes/stonks/+page.svelte
@@ -5,11 +5,163 @@
 	import type { TableSource } from '@skeletonlabs/skeleton';
 	import { tableMapperValues } from '@skeletonlabs/skeleton';
 
-	data.users.forEach((user) => {
-		user.stock = '$' + user.stock;
-	});
+	const normalArrow = '↕';
+	const upArrow = '⬆';
+	const downArrow = '⬇';
+	const TABLE_WIDTH = 5;
 
-	const tableSimple: TableSource = {
+	let tableColumns = ['Name ↕', 'Stock ↕', 'Initial ↕', 'Current ↕', '% Change ↕'];
+	let startTableColumns: string[] = [];
+
+	// assign by value :)
+	for (let i = 0; i < tableColumns.length; i++) {
+		startTableColumns.push(tableColumns[i]);
+	}
+
+	interface StockUser {
+		id: string;
+		name: string;
+		stock: string;
+		initial_price: number;
+		price: number;
+		percent_change: number;
+	}
+
+	if (!data.users[0].stock.includes('$')) {
+		data.users.forEach((user) => {
+			user.stock = '$' + user.stock;
+		});
+	}
+
+	function sortStonkers(sortNum: number) {
+		if (previousSort === sortNum) {
+			sortMod = sortMod * -1;
+		} else {
+			sortMod = 1;
+		}
+
+		fixArrow(sortNum);
+
+		switch (sortNum) {
+			case 1:
+				data.users.sort(sortNames);
+				previousSort = 1;
+				break;
+
+			case 2:
+				data.users.sort(sortStocks);
+				previousSort = 2;
+				break;
+
+			case 3:
+				data.users.sort(sortInitial);
+				previousSort = 3;
+				break;
+
+			case 4:
+				data.users.sort(sortCurrent);
+				previousSort = 4;
+				break;
+
+			case 5:
+				data.users.sort(sortPercent);
+				previousSort = 5;
+				break;
+		}
+
+		//console.log(data.users);
+		data.users = data.users;
+	}
+
+	let sortMod = 1;
+	let previousSort = -100;
+	sortStonkers(5);
+
+	function sortNames(a: StockUser, b: StockUser): number {
+		if (a.name.toUpperCase() < b.name.toUpperCase()) {
+			return 1 * sortMod;
+		}
+		if (a.name.toUpperCase() > b.name.toUpperCase()) {
+			return -1 * sortMod;
+		} else {
+			return 0;
+		}
+	}
+
+	function sortStocks(a: StockUser, b: StockUser): number {
+		if (a.stock.toUpperCase() < b.stock.toUpperCase()) {
+			return 1 * sortMod;
+		}
+		if (a.stock.toUpperCase() > b.stock.toUpperCase()) {
+			return -1 * sortMod;
+		} else {
+			return 0;
+		}
+	}
+
+	function sortInitial(a: StockUser, b: StockUser): number {
+		if (a.initial_price < b.initial_price) {
+			return 1 * sortMod;
+		}
+		if (a.initial_price > b.initial_price) {
+			return -1 * sortMod;
+		} else {
+			return 0;
+		}
+	}
+
+	function sortCurrent(a: StockUser, b: StockUser): number {
+		if (a.price < b.price) {
+			return 1 * sortMod;
+		}
+		if (a.price > b.price) {
+			return -1 * sortMod;
+		} else {
+			return 0;
+		}
+	}
+
+	function sortPercent(a: StockUser, b: StockUser): number {
+		if (a.percent_change < b.percent_change) {
+			return 1 * sortMod;
+		}
+		if (a.percent_change > b.percent_change) {
+			return -1 * sortMod;
+		} else {
+			return 0;
+		}
+	}
+
+	function fixArrow(num: number): void {
+		//console.log(startTableColumns);
+
+		num = num - 1;
+		console.log(tableColumns[num]);
+
+		for (let i = 0; i < tableColumns.length; i++) {
+			if (num !== i) {
+				//console.log(startTableColumns[i]);
+				tableColumns[i] = startTableColumns[i];
+			}
+		}
+
+		console.log(tableColumns[num]);
+
+		if (tableColumns[num].includes(downArrow)) {
+			tableColumns[num] = tableColumns[num].replace(downArrow, upArrow);
+			return;
+		}
+		//console.log(tableColumns[num]);
+
+		tableColumns[num] = tableColumns[num].replace(upArrow, downArrow);
+		tableColumns[num] = tableColumns[num].replace(normalArrow, downArrow);
+
+		//console.log(tableColumns[num]);
+
+		return;
+	}
+
+	/* const tableSimple: TableSource = {
 		// A list of heading labels.
 		head: ['Name', 'Stock', 'Initial', 'Current', 'Percent Change'],
 		// The data visibly shown in your table body UI.
@@ -29,30 +181,50 @@
 			'percent_change'
 		])
 		// Optional: A list of footer labels.
-		/* foot: ['Total', '', '<code class="code">5</code>'] */
-	};
+		foot: ['Total', '', '<code class="code">5</code>'] 
+	}; */
 </script>
 
 <!-- <h1>test</h1>
 <button>test</button> -->
 <br />
 <br />
-<div class="flex justify-center">
+<!-- <div class="flex justify-center">
 	<Table source={tableSimple} interactive={true} regionHead=".table-sort-dsc" />
-</div>
+</div> -->
 
 <!-- Responsive Container (recommended) -->
-<div class="flex justify-center">
+<div class="flex justify-center max-w-md lg:max-w-7xl">
 	<div class="table-container">
 		<!-- Native Table Element -->
 		<table class="table table-interactive">
 			<thead>
 				<tr>
-					<th>Name</th>
-					<th>Stock</th>
-					<th>Initial</th>
-					<th>Current</th>
-					<th>Percent Change</th>
+					<th
+						><button class="text-sm lg:text-lg" on:click={() => sortStonkers(1)}
+							>{tableColumns[0]}</button
+						></th
+					>
+					<th
+						><button class="text-sm lg:text-lg" on:click={() => sortStonkers(2)}
+							>{tableColumns[1]}</button
+						></th
+					>
+					<th
+						><button class="text-sm lg:text-lg" on:click={() => sortStonkers(3)}
+							>{tableColumns[2]}</button
+						></th
+					>
+					<th
+						><button class="text-sm lg:text-lg" on:click={() => sortStonkers(4)}
+							>{tableColumns[3]}</button
+						></th
+					>
+					<th
+						><button class="text-sm lg:text-lg" on:click={() => sortStonkers(5)}
+							>{tableColumns[4]}</button
+						></th
+					>
 				</tr>
 			</thead>
 			<tbody>
@@ -62,7 +234,11 @@
 						<td>{row.stock}</td>
 						<td>{row.initial_price}</td>
 						<td>{row.price}</td>
-						<td>{row.percent_change}</td>
+						{#if row.percent_change > 0}
+							<td class="text-green-500">{row.percent_change}</td>
+						{:else}
+							<td class="text-red-500">{row.percent_change}</td>
+						{/if}
 					</tr>
 				{/each}
 			</tbody>
